99e5daba6298e7bacc37d5c956fadedd56b12fff
Nikos <niqos89@gmail.com>
2025-04-25 20:49:48 +0300

    feat: criteria API queries

--- src/main/java/gr/aueb/cf/App.java
+++ src/main/java/gr/aueb/cf/App.java
@@ -6,6 +6,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.*;
 
 import java.util.List;
 
@@ -56,13 +57,55 @@ public class App {
 //        courses.forEach(System.out::println);
 
         // SELECT teachers και courses που διδασκουν οι teachers
-        String sql = "SELECT t, c.title FROM Teacher t JOIN t.courses c";   // INNER JOIN
-        TypedQuery<Object[]> query = em.createQuery(sql, Object[].class);
-        List<Object[]> results = query.getResultList();
-        for (Object[] result : results) {
-            Teacher teacher = (Teacher) result[0];
-            String courseTitle = (String) result[1];
-            System.out.println("Teacher: " + teacher.getLastname() + " , Course: " + courseTitle);
+//        String sql = "SELECT t, c.title FROM Teacher t JOIN t.courses c";   // INNER JOIN
+//        TypedQuery<Object[]> query = em.createQuery(sql, Object[].class);
+//        List<Object[]> results = query.getResultList();
+//        for (Object[] result : results) {
+//            Teacher teacher = (Teacher) result[0];
+//            String courseTitle = (String) result[1];
+//            System.out.println("Teacher: " + teacher.getLastname() + " , Course: " + courseTitle);
+//        }
+        /*
+         * Criteria API
+         */
+
+//        CriteriaBuilder cb = em.getCriteriaBuilder();
+//        CriteriaQuery<Teacher> query = cb.createQuery(Teacher.class);   // TI ΕΠΙΣΤΡΕΦΕΙ
+//        Root<Teacher> teacher = query.from(Teacher.class);              // FROM
+//
+//        query.select(teacher);      // θα πρεπει να ειναι τοο ιδιο με το CriteriaQuery
+//        List<Teacher> teachers = em.createQuery(query).getResultList();
+//        teachers.forEach(System.out::println);
+
+//        CriteriaBuilder cb = em.getCriteriaBuilder();
+//        CriteriaQuery<String> query = cb.createQuery(String.class);
+//        Root<Course> course = query.from(Course.class);
+//        query.select(course.get("title"));
+//        List<String> titles = em.createQuery(query).getResultList();
+//        titles.forEach(System.out::println);
+
+        // Teachers με επώνυμο 'Μόσχος'
+//        CriteriaBuilder cb = em.getCriteriaBuilder();
+//        CriteriaQuery<Teacher> query = cb.createQuery(Teacher.class);
+//        Root<Teacher> teacher = query.from(Teacher.class);
+//        ParameterExpression<String> lastname  = cb.parameter(String.class);
+//        query.select(teacher).where(cb.equal(teacher.get("lastname"), lastname));
+//        List<Teacher> teachers = em.createQuery(query).setParameter(lastname, "Μόσχος").getResultList();
+//        teachers.forEach(System.out::println);
+
+        // Τα courses (title) και τους teachers (firstname, lastname)
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Object[]> query = cb.createQuery(Object[].class);
+        Root<Course> course = query.from(Course.class);
+        Join<Course, Teacher> teacher = course.join("teacher");
+        query.multiselect(course.get("title"), teacher.get("lastname"), teacher.get("firstname"));
+        List<Object[]> coursesTeachers = em.createQuery(query).getResultList();
+
+        for (Object[] result : coursesTeachers) {
+            String title = (String) result[0];
+            String lastname = (String) result[1];
+            String firstname = (String) result[2];
+            System.out.println(title + ", " + lastname + ", " + firstname);
         }
 
         em.getTransaction().commit();
